71257a2c1a2fcbdbcfc813afdbf34b96b8bee721
adminprecoz <isakrm@outlook.com>
2023-06-01 15:22:39 -0300

    aded

--- Home/urls.py
+++ Home/urls.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from Home import views 
 from . import views
 from . import *
+from .views import SearchResultsView
 
 urlpatterns = [
     path("", views.home, name="Home"),
-    path('json/', views.jsondata),
-    path('pontos/', views.point, name='listar_pontos')
+    path('json/', views.jsondata, name="buscar_preco"),
+    path('pontos/', views.point, name='listar_pontos'),
+    path('search/', SearchResultsView.as_view(), name='search_preco'),
     
 ]
--- Home/views.py
+++ Home/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from .models import Pontos
 from django.http import request
+from django.db.models import Q
+from django.views.generic import TemplateView, ListView
+from django.core import serializers
+import json
+from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home/exemplo.html', {})
@@ -23,4 +28,41 @@ def jsondata(request):
     print(data[:2])
     context = {'data': data}
     return render(request, 'home/exemplo.html', context)
+    
+class SearchResultsView(ListView):
+    model = Pontos
+    template_name = 'home/exemplo.html'
+
+    def get_queryset(request): # new
+        query = request.request.GET.get("q")
+        object_list = Pontos.objects.filter(
+            Q(Nome__icontains=query)
+        )
+        
+        return object_list
+        return render(request, 'home/mainpage.html', object_list)
+
+        qs = serializers.serialize('json', object_list, fields=('latitude', 'longitude', 'Nome', 'preco')[:100])
+        qs = json.loads(qs)
+        
+        print(qs)
+        print(object_list)
+        
+        
+        return HttpResponse(json.dumps({"Nome":qs}), content_type='application/json')
+        
+    
+        
+
+        
+        
+
+
+
+       # return render(request, 'home/exemplo.html', context)
+        #return {"IsActive":IsActive}
+        #return JsonResponse({'object_list':qs})
+        #print(object_list)
+        #
+
     
